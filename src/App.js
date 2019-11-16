7b5565261d9b9304a8ff1297d372741e77e879cd
Hunter Chang <hunter@madwiremedia.com>
2019-11-16 08:07:57 -0700

    reducer with useEffect

--- src/App.js
+++ src/App.js
@@ -1,33 +1,63 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 import "./App.css";
 
 const initialState = {
   title: "Hello World",
-  count: 0
+  count: 0,
+  loading: true,
+  data: null
 };
 
 function reducer(state, action) {
   console.log(action);
-  if (action.type === "add") {
-    return {
-      ...state,
-      count: state.count + 1
-    };
-  }
-  if (action.type === "edit-title") {
-    return {
-      ...state,
-      title: action.title
-    };
+  switch (action.type) {
+    case "SET_DATA": {
+      return {
+        ...state,
+        data: action.data,
+        loading: false
+      };
+    }
+    case "ADD": {
+      return {
+        ...state,
+        count: state.count + 1
+      };
+    }
+    case "SUBTRACT": {
+      return {
+        ...state,
+        count: state.count - 1
+      };
+    }
+    case "EDIT_TITLE": {
+      return {
+        ...state,
+        title: action.title
+      };
+    }
+    default:
+      return state;
   }
 }
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
   console.log(state);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then(res => res.json())
+      .then(data => {
+        dispatch({ type: "SET_DATA", data });
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
+        {state.loading && <p>LOADING...</p>}
+        {state.data &&
+          state.data.map(todo => <p key={todo.id}>{todo.title}</p>)}
         <h1>{state.title}</h1>
         <div>
           <input
@@ -35,7 +65,7 @@ function App() {
             value={state.title}
             onChange={e => {
               dispatch({
-                type: "edit-title",
+                type: "EDIT_TITLE",
                 title: e.target.value
               });
             }}
@@ -46,11 +76,21 @@ function App() {
           type="button"
           onClick={() => {
             dispatch({
-              type: "add"
+              type: "ADD"
             });
           }}
         >
           Add Reducer Count
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            dispatch({
+              type: "SUBTRACT"
+            });
+          }}
+        >
+          Subtract Reducer Count
         </button>
       </header>
     </div>
